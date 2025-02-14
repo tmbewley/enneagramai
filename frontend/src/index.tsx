4c431efb7eaec09f0be0465680f3162e4da4075b
tmbewley <tmbewley@gmail.com>
2025-02-14 15:09:25 -0600

    fix: Simplify build configuration and use root-level environment variables

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AuthProvider } from './contexts/AuthContext';
 
-const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find the root element');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
